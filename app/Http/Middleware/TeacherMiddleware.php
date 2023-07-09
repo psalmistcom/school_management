9308b671b7c92d00a1a906dfdc49c5735d406d4e
psalmistcom <dadewale9@gmail.com>
2023-07-09 18:26:01 +0100

    protected pages

--- app/Http/Middleware/TeacherMiddleware.php
+++ app/Http/Middleware/TeacherMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class TeacherMiddleware
@@ -15,6 +16,17 @@ class TeacherMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request);
+        if (!empty(Auth::check())) {
+            $userType = Auth::user()->user_type;
+            if ($userType == 2) {
+                return $next($request);
+            } else {
+                Auth::logout();
+                return redirect(url(''));
+            }
+        } else {
+            Auth::logout();
+            return redirect(url(''));
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -23,14 +23,34 @@ Route::get('/login', [AuthController::class, 'login']);
 Route::post('/login', [AuthController::class, 'authLogin']);
 Route::get('/logout', [AuthController::class, 'logout']);
 
+Route::get('/admin/admin/list', function () {
+    return view('admin.admin.list');
+});
 
 // Admin Routes 
-
-Route::group(['prefix' => 'admin'], function () {
-    Route::get('/dashboard', function () {
+Route::group(['middleware' => 'admin'], function () {
+    Route::get('admin/dashboard', function () {
         return view('admin.dashboard');
     });
-    Route::get('/admin/list', function () {
-        return view('admin.admin.list');
+});
+
+//Teacher Routes
+Route::group(['middleware' => 'teacher'], function () {
+    Route::get('teacher/dashboard', function () {
+        return view('admin.dashboard');
+    });
+});
+
+// Student Route 
+Route::group(['middleware' => 'student'], function () {
+    Route::get('student/dashboard', function () {
+        return view('admin.dashboard');
+    });
+});
+
+// Parent Route 
+Route::group(['middleware' => 'parent'], function () {
+    Route::get('parent/dashboard', function () {
+        return view('admin.dashboard');
     });
 });
